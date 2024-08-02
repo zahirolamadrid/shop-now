a20b2de9947a2c44318583929fb00dcd263a89da
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-02 16:51:27 -0400

    Get and show all product with title and price

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <RouterLink class="navbar-brand" href="/" to="/">Shop Now!</RouterLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink class="nav-link" aria-current="page" href="/" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" aria-current="page" href="/products" to="/products">Products</RouterLink>
+        </li>
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
     <h1>Products</h1>
+
+    <div>
+    <div v-for="product in products" :key="product.id">
+      <h3>{{ product.title }}</h3>
+      <p>{{ product.price }}</p>
+      </div>
+  </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      products: []
+    }
+  },
+  created() {
+    axios.get('https://dummyjson.com/products')
+      .then(response => {
+        this.products = response.data.products;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
 
 <style>
 </style>
