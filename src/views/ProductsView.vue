7f946ce61d9ef48c80a486569b2e2ad82a8f0f58
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-02 19:01:15 -0400

    Add buttons for cart and details

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -4,13 +4,17 @@
 
     <div class="container">
     <div class="row">
-      <div class="col-md-4" v-for="product in products" :key="product.id">
+      <div class="col-md-4 my-3" v-for="product in products" :key="product.id">
         <div class="card">
           <img :src="product.thumbnail" class="card-img-top" :alt="product.title">
           <div class="card-body">
             <h5 class="card-title">{{ product.title }}</h5>
             <p class="card-text">{{ product.description }}</p>
             <p class="card-text">Price: ${{ product.price }}</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+              <button class="btn btn-secondary me-md-2" type="button">Add to Cart</button>
+              <RouteLink to="/details" class="btn btn-primary" type="button">Details</RouteLink>
+            </div>
           </div>
         </div>
       </div>
