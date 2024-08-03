b9364190046e6e41989521f0b100bc8dedc72968
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-03 10:44:49 -0400

    Add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav aria-label="Page navigation">
+        <ul class="pagination justify-content-center">
+            <li class="page-item" :class="{ disabled: currentPage === 1 }">
+                <a class="page-link" href="#" @click.prevent="prevPage">Previous</a>
+            </li>
+            <li class="page-item" v-for="page in pages" :key="page" :class="{ active: page === currentPage }">
+                <a class="page-link" href="#" @click="setPage(page)">{{ page }}</a>
+            </li>
+            <li class="page-item" :class="{ disabled: currentPage === totalPages }">
+                <a class="page-link" href="#" @click.prevent="nextPage">Next</a>
+            </li>
+        </ul> 
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        currentPage: {
+            type: Number,
+            required: true
+        },
+        totalPages: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        pages() {
+            const pageCount = this.totalPages;
+            const pageRange = [];
+            for (let i = 1; i <= pageCount; i++) {
+                pageRange.push(i);
+            }
+            return pageRange;
+        }
+    },
+    methods: {
+        prevPage() {
+            this.$emit('update:currentPage', this.currentPage - 1);
+        },
+        nextPage() {
+            this.$emit('update:currentPage', this.currentPage + 1);
+        },
+        setPage(page) {
+            this.$emit('update:currentPage', page);
+        }
+    }
+};
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,45 +1,71 @@
 <template>
-  <div><div class="container">
-    <div class="row">
-      <div class="col-md-4 my-3" v-for="product in products" :key="product.id">
-        <div class="card">
-          <img :src="product.thumbnail" class="card-img-top" :alt="product.title">
-          <div class="card-body">
-            <h5 class="card-title">{{ product.title }}</h5>
-            <p class="card-text">{{ product.description }}</p>
-            <p class="card-text">Price: ${{ product.price }}</p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-              <button class="btn btn-secondary me-md-2" type="button">Add to Cart</button>
-              <RouterLink :to="{ name: 'product-details', params: {id: product.id}}" class="btn btn-primary" type="button">View Product</RouterLink>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 my-3" v-for="product in products" :key="product.id">
+          <div class="card">
+            <img :src="product.thumbnail" class="card-img-top" :alt="product.title">
+            <div class="card-body">
+              <h5 class="card-title">{{ product.title }}</h5>
+              <p class="card-text">{{ product.description }}</p>
+              <p class="card-text">Price: ${{ product.price }}</p>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button class="btn btn-secondary me-md-2" type="button">Add to Cart</button>
+                <RouterLink :to="{ name: 'product-details', params: { id: product.id } }" class="btn btn-primary"
+                  type="button">View Product</RouterLink>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <div>
+    <Pagination :current-page="currentPage" :total-pages="totalPages" @update:currentPage="currentPage = $event"></Pagination>
   </div>
+
 </template>
+
+
 
 <script>
 import axios from 'axios';
+import Pagination from './../components/Pagination.vue';
 
 export default {
+  components: { Pagination },
   data() {
     return {
-      products: []
+      products: [],
+      limit: 12,
+      currentPage: 1,
+      totalPages: 0,
+      skip: 0
     }
   },
-  created() {
-    axios.get('https://dummyjson.com/products')
-      .then(response => {
-        this.products = response.data.products;
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  methods: {
+    fetchProducts() {
+      this.skip = (this.currentPage - 1) * this.limit;
+      axios.get('https://dummyjson.com/products?limit=' + this.limit + '&skip=' + this.skip)
+        .then(response => {
+          this.products = response.data.products;
+          this.totalPages = response.data.total / this.limit;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  watch: {
+    currentPage() {
+      this.fetchProducts();
+    }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
