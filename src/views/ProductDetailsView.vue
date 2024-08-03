c5c0052eb9f80a531603f986e0d1754102630e92
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-03 13:59:47 -0400

    Best design on product details

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="card">
-    <img :src="product.images" class="card-img-top" :alt="product.title">
-    <div class="card-body">
-      <h5 class="card-title">{{ product.title }}</h5>
-      <p class="card-text">{{ product.description }} {{ $route.params.username }}</p>
-      <p class="card-text">Price: ${{ product.price }}</p>
-      <button class="btn btn-primary">Add to Cart</button>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <img :src="product.images" class="img-fluid" :alt="product.title">
+      </div>
+      <div class="col-md-6">
+        <h1 class="h2">{{ product.title }}</h1>
+        <p class="card-text">{{ product.description }}</p>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+          <p class="text-muted">
+            <span class="mx-2" v-if="product.discountPercentage > 0">
+              <s>${{ Math.round((100 * product.price) / (100 - product.discountPercentage) *100) / 100 }}</s>
+            </span>
+            <span class="font-weight-bold">
+              <strong>${{ product.price }}</strong>
+            </span>
+          </p>
+        </div>
+        <div v-if="product.availabilityStatus === 'Low Stock'" class="alert alert-warning" role="alert">
+          Low Stock! Only {{ product.stock }} remaining.
+        </div>
+        <button class="btn btn-primary">Add to Cart</button>
+      </div>
     </div>
   </div>
 </template>
