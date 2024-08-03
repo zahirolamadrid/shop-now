31787835cd2e13a8789bc0f60f989c8202174096
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-03 12:12:26 -0400

    Get product details

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductDetailsView from '../views/ProductDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,10 +21,7 @@ const router = createRouter({
     {
       path: '/products/:id',
       name: 'product-details',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProductDetailsView.vue')
+      component: ProductDetailsView
     }
   ]
 })
--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -1,8 +1,33 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    <h1>Details</h1>
+  <div class="card">
+    <img :src="product.images" class="card-img-top" :alt="product.title">
+    <div class="card-body">
+      <h5 class="card-title">{{ product.title }}</h5>
+      <p class="card-text">{{ product.description }} {{ $route.params.username }}</p>
+      <p class="card-text">Price: ${{ product.price }}</p>
+      <button class="btn btn-primary">Add to Cart</button>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      product: {}
+    };
+  },
+  created() {
+      axios.get('https://dummyjson.com/products/' + this.$route.params.id)
+        .then(response => {
+          this.product = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+};
+</script>
+
