bba4703117d47f6bd7654852355d650ee2a4f3d0
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-03 20:10:49 -0400

    Use products as home

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductDetailsView from '../views/ProductDetailsView.vue'
 import SearchView from '../views/SearchView.vue'
+import ProductsView from '../views/ProductsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,15 +10,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: ProductsView
     },
     {
       path: '/products',
       name: 'products',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProductsView.vue')
+      component: ProductsView
     },
     {
       path: '/products/:id',
