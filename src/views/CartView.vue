913cef7fa7c940bbb3f7941e49b82d3db748ecd4
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-04 09:36:05 -0400

    Fix the remove item function

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,62 +1,59 @@
 <template>
-    <div class="container">
-      <h1 class="text-center">Shopping Cart</h1>
-      <div class="row">
-        <div class="col-md-8">
-          <div class="card mb-3" v-for="item in cartItems" :key="item.product.id">
-            <div class="card-body">
-              <div class="d-flex justify-content-between">
-                <div>
-                  <h6>{{ item.product.title }}</h6>
-                  <p class="card-text">{{ item.product.description }}</p>
-                </div>
-                <div>
-                  <input type="number" v-model="item.quantity" min="1" class="form-control" style="width: 60px;">
-                  <button class="btn btn-danger btn-sm" @click="removeItem(item.product.id)">Remove</button>
-                </div>
+  <div class="container">
+    <h1 class="text-center">Shopping Cart</h1>
+    <div class="row">
+      <div class="col-md-8">
+        <div class="card mb-3" v-for="item in cartItems" :key="item.product.id">
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <div>
+                <h6>{{ item.product.title }}</h6>
+                <p class="card-text">{{ item.product.description }}</p>
               </div>
-              <p class="card-text">Price: ${{ item.product.price * item.quantity }}</p>
-              <router-link :to="{ name: 'product-details', params: { id: item.product.id } }" class="btn btn-primary btn-sm">View Product</router-link>
+              <div>
+                <input type="number" v-model="item.quantity" min="1" class="form-control" style="width: 60px;">
+                <button class="btn btn-danger btn-sm" @click="removeItem(item.product.id)">Remove</button>
+              </div>
             </div>
+            <p class="card-text">Price: ${{ item.product.price * item.quantity }}</p>
+            <router-link :to="{ name: 'product-details', params: { id: item.product.id } }"
+              class="btn btn-primary btn-sm">View Product</router-link>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Cart Summary</h5>
-              <ul class="list-group">
-                <li class="list-group-item">Subtotal: ${{ calculateSubtotal() }}</li>
-                <li class="list-group-item">Total: ${{ calculateSubtotal() }}</li>
-              </ul>
-              <button class="btn btn-success btn-block">Checkout</button>
-            </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Cart Summary</h5>
+            <ul class="list-group">
+              <li class="list-group-item">Total: ${{ calculateTotal() }}</li>
+            </ul>
+            <button class="btn btn-success btn-block">Checkout</button>
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { useCartStore } from './../stores/cart';
+  </div>
+</template>
 
-  export default {
+<script>
+import { useCartStore } from './../stores/cart';
 
-    data() {
-      const cartStore = useCartStore();
+export default {
 
-      return {
-        cartItems: cartStore.cartItems
-      };
+  data() {
+    return {
+      cartItems: useCartStore().cartItems
+    };
+  },
+  methods: {
+    calculateTotal() {
+      return this.cartItems.reduce((total, item) => total + (item.product.price * item.quantity), 0);
     },
-    methods: {
-      removeItem(productId) {
-        // Remove the item from the cartItems array
-        this.cartItems = this.cartItems.filter(item => item.product.id !== productId);
-      },
-      calculateSubtotal() {
-        // Calculate the total price based on cart items
-        return this.cartItems.reduce((total, item) => total + (item.product.price * item.quantity), 0);
-      }
+    removeItem(productId) {
+      useCartStore().removeItem(productId);
+      this.cartItems = useCartStore().cartItems
     }
-  };
-  </script>
+  }
+};
+</script>
