c2ad2efc48a20159e324313a966a7ddd02483abe
Zahiro Lamadrid <z.lamadrid@kapital.mx>
2024-08-04 19:58:27 -0400

    Clean the nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,11 @@
 <template>
   <nav class="navbar navbar-expand bg-body-tertiary">
     <div class="container-fluid">
-      <RouterLink class="navbar-brand" href="/" to="/">Shop Now!</RouterLink>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link" aria-current="page" to="/"><strong>Shop Now!</strong></RouterLink>
+          </li>
           <li class="nav-item">
             <RouterLink class="nav-link" aria-current="page" to="/products">Products</RouterLink>
           </li>
